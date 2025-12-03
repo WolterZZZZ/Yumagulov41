fa325d3addb6f46c88ea5f2e0ed17807c6bf05d5
Sa4ai <ar.eec@mail.ru>
2025-12-03 15:52:29 +0500

    12 лаба сортировка

--- ServicePage.xaml.cs
+++ ServicePage.xaml.cs
@@ -25,11 +25,70 @@ namespace Yumagulov41
             InitializeComponent();
             var currentServices = Yumagulov41Entities.GetContext().Product.ToList();
             ServiceListView.ItemsSource = currentServices;
+            ComboType.SelectedIndex = 0;
+            UpdateServices();
         }
+        private void UpdateServices()
+        {
+            var currentServices = Yumagulov41Entities.GetContext().Product.ToList();
 
+            if(ComboType.SelectedIndex == 0)
+            {
+                currentServices = currentServices.Where(p => p.ProductDiscountAmount >= 0 && p.ProductDiscountAmount <= 100).ToList(); 
+            }
+            if (ComboType.SelectedIndex == 1)
+            {
+                currentServices = currentServices.Where(p => p.ProductDiscountAmount >= 0 && p.ProductDiscountAmount <= 9).ToList();
+            }
+            if (ComboType.SelectedIndex == 2) {
+                currentServices = currentServices.Where(p => p.ProductDiscountAmount >= 10 && p.ProductDiscountAmount <= 14).ToList();
+            }
+            if(ComboType.SelectedIndex == 3)
+            {
+                currentServices = currentServices.Where(p => p.ProductDiscountAmount >= 15 && p.ProductDiscountAmount <= 100).ToList();
+            }
+            currentServices = currentServices.Where(p => p.ProductName.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+            ServiceListView.ItemsSource = currentServices.ToList();
+
+            if(RButtonDown.IsChecked.Value)
+            {
+                ServiceListView.ItemsSource = currentServices.OrderByDescending(p => p.ProductCost).ToList();
+
+            }
+            if (RButtonUp.IsChecked.Value)
+            {
+                ServiceListView.ItemsSource = currentServices.OrderBy(p => p.ProductCost).ToList();
+            }
+        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Manager.MainFrame.Navigate(new AddEditPage());
+            UpdateServices();
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void RButtonUp_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void RButtonDown_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateServices();
         }
     }
 }
